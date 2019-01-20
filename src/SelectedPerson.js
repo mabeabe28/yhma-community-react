a44dba4f5345870cdec1185c4b73fa36f2e1d140
Paolo <mabeabe28@gmail.com>
2019-01-20 21:21:01 +0000

    introduced main text

--- src/SelectedPerson.js
+++ src/SelectedPerson.js
@@ -6,19 +6,26 @@ class SelectedPerson extends Component {
   render() {
     const personObj = this.props.person;
 
+    let mainText = "YHMA: Community";
+    let description = "Spotlighting individuals with a cause, creative and talented members from the community.";
+
+    if(this.props.person.yhmac_id !== undefined){
+      mainText = personObj.name;
+      description = personObj.description;
+    }
+
+
     return (
       <div className="yhmac-selected__container">
-      {(this.props.person.yhmac_id !== undefined) && (
         <div className="yhmac-person-text__wrapper">
           <div key={personObj.yhmac_id} className={`yhmac-person-text selected reveal-text-animation`}>
-            <h1>{personObj.name}</h1>
+            <h1>{mainText}</h1>
           </div>
           <div key={`${personObj.yhmac_id}--desc`} className="desc reveal-text-animation">
-            {personObj.description}
+            {description}
           </div>
         </div>
 
-      )}
         {/*<img className="fit" src={personObj.user_photo} alt={personObj.name}/>*/}
       </div>
     );
