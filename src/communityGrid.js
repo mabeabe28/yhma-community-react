f81a34a87da536ba0b4ca4422f58a91703b81f0a
Paolo <mabeabe28@gmail.com>
2019-01-24 00:29:04 +0000

    scroll button implementation

--- src/communityGrid.js
+++ src/communityGrid.js
@@ -10,9 +10,33 @@ class communityGrid extends Component {
     super(props);
 
     this.handleClick = this.handleClick.bind(this);
-    this.myRef = React.createRef();
+    this.gridWindowRef = React.createRef();
+    this.handleScroll = this.handleScroll.bind(this);
     this.state = {
-      selected : {}
+      selected : {},
+      showLeftScroll:false,
+      showRightScroll:true
+    }
+  }
+
+  componentDidMount() {
+    document.getElementById(`gridWindow`).addEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll(){
+    const offsetLeft = document.getElementById(`gridWindow`).offsetLeft;
+    const leftPosition = document.getElementById(`gridWindow`).scrollLeft;
+
+    if(leftPosition > 0){
+      this.setState({showLeftScroll:true});
+    }
+
+    if(leftPosition === 0){
+     this.setState({showLeftScroll:false});
+    }
+
+    if(leftPosition === offsetLeft){
+     this.setState({showRightScroll:false});
     }
   }
 
@@ -25,25 +49,44 @@ class communityGrid extends Component {
       this.setState({selected:{}});
     }
   }
+  gridScroll(direction){
+    //console.log('scroll',  this.gridWindowRef);
+    //console.log(document.getElementById(`gridWindow`).getBoundingClientRect().left);
+    //console.log(document.getElementById(`gridWindow`).getBoundingClientRect().right);
 
+    if(direction === 'right')
+      this.gridWindowRef.current.scrollLeft += 400;
+    else if(direction === 'left')
+      this.gridWindowRef.current.scrollLeft -= 400;
+  }
   render(props) {
     //console.log(this);
     return (
       <div>
         <div className="yhmac-grid">
-              <ul className="gridWrapper">
+              <ul id="gridWindow" className="gridWrapper" ref={this.gridWindowRef}>
               {this.props.data.map((person, i) => (
                   <li key={i} className="gridItem">
                     <Person
                       person={person}
                       handleClick={this.handleClick}
                       selected={this.state.selected}
-                      ref={this.myRef}
                     />
                   </li>
               ))}
               </ul>
+              <div className="yhmac-grid__scroll">
+                <div className="yhmac-grid__scroll-button-container">
+                {this.state.showLeftScroll &&
+                  <span className="yhmac-grid__scroll-button yhmac-grid__scroll-button--left" onClick={()=>this.gridScroll('left')}>></span>
+                }
+                {this.state.showRightScroll &&
+                  <span className="yhmac-grid__scroll-button yhmac-grid__scroll-button--right" onClick={()=>this.gridScroll('right')}>></span>
+                }
+                </div>
+              </div>
         </div>
+
         <div className="yhmac-issue">
           <IssueNo
             person={this.state.selected}
